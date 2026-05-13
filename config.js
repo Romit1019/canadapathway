876521964ce26f251fe4a52be1437107f3dc9d40
Romit1019 <romit4job@gmail.com>
2026-05-13 18:40:45 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Key injected at deploy time by GitHub Actions.
-window.BREVO_KEY = '';
+window.BREVO_KEY = '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';
 window.BREVO_LIST_ID = 2;
 window.BREVO_RCIC_LIST_ID = 3;
